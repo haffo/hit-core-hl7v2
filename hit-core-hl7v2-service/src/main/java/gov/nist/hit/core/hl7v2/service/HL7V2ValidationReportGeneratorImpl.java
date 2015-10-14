557c5633ed21250e9ac41fee09ec50ac361ccbb6
Affo, Harold <affoharold@yahoo.fr>
2015-10-14 11:22:16 -0400

    Fixed report generator service

--- hit-core-hl7v2-service/src/main/java/gov/nist/hit/core/hl7v2/service/HL7V2ValidationReportGeneratorImpl.java
+++ hit-core-hl7v2-service/src/main/java/gov/nist/hit/core/hl7v2/service/HL7V2ValidationReportGeneratorImpl.java
@@ -11,105 +11,13 @@
  */
 package gov.nist.hit.core.hl7v2.service;
 
-import gov.nist.healthcare.unified.converters.XMLConverter;
-import gov.nist.healthcare.unified.model.EnhancedReport;
-import gov.nist.hit.core.service.ValidationReportGenerator;
-import gov.nist.hit.core.service.exception.ValidationReportException;
-
-import java.io.IOException;
-
-import org.apache.commons.io.IOUtils;
 
 /**
  * @author Harold Affo (NIST)
  */
 
-public class HL7V2ValidationReportGeneratorImpl extends ValidationReportGenerator {
-
-  private static final String HTML_XSL = "/xslt/report-html.xsl";
-  private static final String PDF_XSL = "/xslt/report-pdf.xsl";
-  private static final String CSS = "/report.css";
-  private String css = "";
-
-  public HL7V2ValidationReportGeneratorImpl() {
-    try {
-      css = IOUtils.toString(HL7V2ValidationReportGeneratorImpl.class.getResourceAsStream(CSS));
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
+public class HL7V2ValidationReportGeneratorImpl extends HL7V2ValidationReportGenerator {
 
 
-  @Override
-  public String toHTML(String xml) throws ValidationReportException {
-    try {
-      return addCss(super.toHTML(xml));
-    } catch (IOException e) {
-      throw new ValidationReportException(e);
-    }
-  }
-
-
-
-  @Override
-  public String toXHTML(String xml) throws ValidationReportException {
-    try {
-      return addCss(super.toXHTML(xml));
-    } catch (IOException e) {
-      throw new ValidationReportException(e);
-    }
-  }
-
-
-
-  /**
-   * @param htmlReport
-   * @return
-   * @throws IOException
-   */
-  private String addCss(String htmlReport) throws IOException {
-    StringBuffer sb = new StringBuffer();
-    sb.append("<html xmlns='http://www.w3.org/1999/xhtml'>");
-    sb.append("<head>");
-    sb.append("<title>HL7 V2 Message Validation Report</title>");
-    sb.append("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />");
-    sb.append("<style>");
-    sb.append(css);
-    sb.append("</style></head><body>");
-    sb.append(htmlReport);
-    sb.append("</body></html>");
-    return sb.toString();
-  }
-
-
-
-  @Override
-  public String getPdfConversionXslt() {
-    try {
-      return IOUtils.toString(ValidationReportGenerator.class.getResourceAsStream(PDF_XSL));
-    } catch (IOException e) {
-      throw new ValidationReportException(e.getMessage());
-    }
-  }
-
-  @Override
-  public String getHtmlConversionXslt() {
-    try {
-      return IOUtils.toString(HL7V2ValidationReportGeneratorImpl.class
-          .getResourceAsStream(HTML_XSL));
-    } catch (IOException e) {
-      throw new ValidationReportException(e.getMessage());
-    }
-  }
-
-
-
-  @Override
-  public String toXML(String json) throws Exception {
-    EnhancedReport report = EnhancedReport.from("json", json);
-    String xml = new XMLConverter().convert(report);
-    return xml;
-
-  }
 
 }

bd6a2a6ee74a5a712aea9f6c88363df4da98627e
Affo, Harold <affoharold@yahoo.fr>
2016-07-19 15:05:32 -0400

    add unit test for profile viewer

--- hit-core-hl7v2-service/src/main/java/gov/nist/hit/core/hl7v2/service/HL7V2ProfileParser.java
+++ hit-core-hl7v2-service/src/main/java/gov/nist/hit/core/hl7v2/service/HL7V2ProfileParser.java
@@ -11,6 +11,29 @@
  */
 package gov.nist.hit.core.hl7v2.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.apache.commons.io.IOUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import gov.nist.hit.core.domain.ProfileElement;
 import gov.nist.hit.core.domain.ProfileModel;
 import gov.nist.hit.core.domain.constraints.ByID;
@@ -40,30 +63,6 @@ import hl7.v2.profile.SegmentRef;
 import hl7.v2.profile.Usage;
 import hl7.v2.profile.ValueSetSpec;
 import hl7.v2.profile.XMLDeserializer;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
-import java.util.regex.Pattern;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
-
-import org.apache.commons.io.IOUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
 import scala.collection.Iterator;
 import scala.collection.immutable.List;
 
@@ -85,9 +84,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
 
   @Override
   /**
-   * integrationProfileXml: integration profile xml content 
-   * conformanceProfileId: conformance profile id
-   * Options: constraints xml content
+   * integrationProfileXml: integration profile xml content conformanceProfileId: conformance
+   * profile id Options: constraints xml content
    */
   public ProfileModel parse(String integrationProfileXml, String conformanceProfileId,
       String... constraints) throws ProfileParserException {
@@ -125,8 +123,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
       throws ProfileParserException {
     try {
       if (!(conformanceProfile instanceof Message)) {
-        throw new IllegalArgumentException("Conformance Profile is not a valid instanceof "
-            + Message.class.getCanonicalName());
+        throw new IllegalArgumentException(
+            "Conformance Profile is not a valid instanceof " + Message.class.getCanonicalName());
       }
       String c1Xml = constraints != null && constraints.length > 0 ? constraints[0] : null;
       String c2Xml = constraints != null && constraints.length > 1 ? constraints[1] : null;
@@ -198,18 +196,19 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
     return c1;
   }
 
-  private ProfileElement process(Message m) throws XPathExpressionException,
-      CloneNotSupportedException {
+  private ProfileElement process(Message m)
+      throws XPathExpressionException, CloneNotSupportedException {
     model = new ProfileModel();
     ProfileElement message = new ProfileElement("FULL");
     message.setType(TYPE_MESSAGE);
     message.setRelevent(true);
     message.setId(m.id());
     model.setMessage(message);
-    message.setConformanceStatements(this.findConformanceStatements(this.conformanceStatements
-        .getMessages(), model.getMessage().getId(), model.getMessage().getName()));
-    message.setPredicates(this.findPredicates(this.predicates.getMessages(), model.getMessage()
-        .getId(), model.getMessage().getName()));
+    message.setConformanceStatements(
+        this.findConformanceStatements(this.conformanceStatements.getMessages(),
+            model.getMessage().getId(), model.getMessage().getName()));
+    message.setPredicates(this.findPredicates(this.predicates.getMessages(),
+        model.getMessage().getId(), model.getMessage().getName()));
 
     scala.collection.immutable.List<SegRefOrGroup> children = m.structure();
     if (children != null && !children.isEmpty()) {
@@ -307,8 +306,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
   }
 
   private boolean relevent(String usage, boolean hide) {
-    return (usage == null || usage.equals("R") || usage.equals("RE") || usage.equals("C") || (usage
-        .startsWith("C") && (usage.contains("R") || usage.contains("RE")))) && !hide;
+    return (usage == null || usage.equals("R") || usage.equals("RE") || usage.equals("C")
+        || (usage.startsWith("C") && (usage.contains("R") || usage.contains("RE")))) && !hide;
   }
 
   /**
@@ -322,8 +321,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
    * @throws XPathExpressionException
    * @throws CloneNotSupportedException
    */
-  private ProfileElement process(Segment s, Req req) throws XPathExpressionException,
-      CloneNotSupportedException {
+  private ProfileElement process(Segment s, Req req)
+      throws XPathExpressionException, CloneNotSupportedException {
     ProfileElement element = new ProfileElement();
     element.setName(s.name());
     element.setType(TYPE_SEGMENT);
@@ -332,8 +331,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
     element.setId(s.id());
     element.setDynamicMaps(dynaMap(s));
     element.setPredicates(this.findPredicates(this.predicates.getSegments(), s.id(), s.name()));
-    element.setConformanceStatements(this.findConformanceStatements(
-        this.conformanceStatements.getSegments(), s.id(), s.name()));
+    element.setConformanceStatements(
+        this.findConformanceStatements(this.conformanceStatements.getSegments(), s.id(), s.name()));
 
     scala.collection.immutable.List<Field> children = s.fields();
     if (children != null && !children.isEmpty()) {
@@ -346,8 +345,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
   }
 
 
-  private Map<Integer, Set<String>> dynaMap(Segment s) throws XPathExpressionException,
-      CloneNotSupportedException {
+  private Map<Integer, Set<String>> dynaMap(Segment s)
+      throws XPathExpressionException, CloneNotSupportedException {
     List<DynMapping> dynamicMappings = s.mappings();
     if (!dynamicMappings.isEmpty()) {
       Map<Integer, Set<String>> maps = new HashMap<Integer, Set<String>>();
@@ -391,8 +390,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
     element.setPosition(req.position() + "");
     element.setId(g.id());
     element.setPredicates(this.findPredicates(this.predicates.getGroups(), g.id(), g.name()));
-    element.setConformanceStatements(this.findConformanceStatements(
-        this.conformanceStatements.getGroups(), g.id(), g.name()));
+    element.setConformanceStatements(
+        this.findConformanceStatements(this.conformanceStatements.getGroups(), g.id(), g.name()));
 
     // String targetPath = getTargetPath(element);
     // if (!targetPath.equals("")) {
@@ -425,8 +424,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
     if (element == null || element.getType().equals(TYPE_MESSAGE))
       return "";
     String pTarget = getTargetPath(element.getParent());
-    return pTarget.equals("") ? element.getPosition() + "[1]" : pTarget + "."
-        + element.getPosition() + "[1]";
+    return pTarget.equals("") ? element.getPosition() + "[1]"
+        : pTarget + "." + element.getPosition() + "[1]";
   }
 
   /**
@@ -542,8 +541,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
    * @throws XPathExpressionException
    * @throws CloneNotSupportedException
    */
-  private void process(Field f, ProfileElement parent) throws XPathExpressionException,
-      CloneNotSupportedException {
+  private void process(Field f, ProfileElement parent)
+      throws XPathExpressionException, CloneNotSupportedException {
     if (f == null)
       return;
     ProfileElement element = process(f.req(), new ProfileElement(), parent);
@@ -622,8 +621,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
   }
 
 
-  private ProfileElement process(Datatype d) throws XPathExpressionException,
-      CloneNotSupportedException {
+  private ProfileElement process(Datatype d)
+      throws XPathExpressionException, CloneNotSupportedException {
     if (!datatypesMap.containsKey(d.id())) {
       ProfileElement element = new ProfileElement();
       element.setId(d.id());
@@ -633,8 +632,8 @@ public abstract class HL7V2ProfileParser extends ProfileParser {
       // element.setIcon(ICON_DATATYPE);
       element.setRelevent(true);
       element.setPredicates(this.findPredicates(this.predicates.getDatatypes(), d.id(), d.name()));
-      element.setConformanceStatements(this.findConformanceStatements(
-          this.conformanceStatements.getDatatypes(), d.id(), d.name()));
+      element.setConformanceStatements(this
+          .findConformanceStatements(this.conformanceStatements.getDatatypes(), d.id(), d.name()));
       datatypesMap.put(d.id(), element);
       if (d instanceof Composite) {
         Composite c = (Composite) d;
--- hit-core-hl7v2-service/src/test/java/gov/nist/hit/core/hl7v2/service/profile/unit/ProfileParserImplTest.java
+++ hit-core-hl7v2-service/src/test/java/gov/nist/hit/core/hl7v2/service/profile/unit/ProfileParserImplTest.java
@@ -1,31 +1,62 @@
 package gov.nist.hit.core.hl7v2.service.profile.unit;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import gov.nist.hit.core.domain.ProfileElement;
 import gov.nist.hit.core.domain.ProfileModel;
 import gov.nist.hit.core.hl7v2.service.HL7V2ProfileParserImpl;
 import gov.nist.hit.core.service.ProfileParser;
 import gov.nist.hit.core.service.exception.ProfileParserException;
-
-import java.io.IOException;
-
-import org.apache.commons.io.IOUtils;
-import org.junit.Test;
 
 
 public class ProfileParserImplTest {
 
   ProfileParser parser = new HL7V2ProfileParserImpl();
 
+  //
+  // @Test
+  // public void testParse() throws ProfileParserException, IOException {
+  // String profile =
+  // IOUtils.toString(ProfileParserImplTest.class
+  // .getResourceAsStream("/profiles/1_1_1_Profile.xml"));
+  // String constraints =
+  // IOUtils.toString(ProfileParserImplTest.class
+  // .getResourceAsStream("/constraints/1_1_1_Constraints.xml"));
+  // ProfileModel model = parser.parse(profile, "bfb1c703-c96e-4f2b-8950-3f5b1c9cd2d8",
+  // constraints);
+  //
+  // }
+
+
   @Test
-  public void testParse() throws ProfileParserException, IOException {
-    String profile =
-        IOUtils.toString(ProfileParserImplTest.class
-            .getResourceAsStream("/profiles/1_1_1_Profile.xml"));
-    String constraints =
-        IOUtils.toString(ProfileParserImplTest.class
-            .getResourceAsStream("/constraints/1_1_1_Constraints.xml"));
-    ProfileModel model = parser.parse(profile, "bfb1c703-c96e-4f2b-8950-3f5b1c9cd2d8", constraints);
+  public void testParseLRIProfile() throws ProfileParserException, IOException {
+    String profile = IOUtils
+        .toString(ProfileParserImplTest.class.getResourceAsStream("/profiles/1_1_2_Profile.xml"));
+    String constraints = IOUtils.toString(
+        ProfileParserImplTest.class.getResourceAsStream("/constraints/1_1_2_Constraints.xml"));
+    ProfileModel model = parser.parse(profile, "ORU_R01:LRI_GU_FRN", constraints);
+    ProfileElement message = model.getMessage();
+    ProfileElement group = message.getChildren().get(2);
+    assertEquals("PATIENT_RESULT", group.getName());
+    group = group.getChildren().get(1);
+    assertEquals("ORDER_OBSERVATION", group.getName());
+    List<gov.nist.hit.core.domain.constraints.Predicate> predicates = group.getPredicates();
+    assertFalse(predicates.size() == 0);
+    group = group.getChildren().get(5);
+    assertEquals("OBSERVATION", group.getName());
+    predicates = group.getPredicates();
+    assertFalse(predicates.size() == 0);
+
 
   }
+
 
 
 }

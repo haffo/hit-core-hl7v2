19ca33ef386c6b7cf915cc958ba98eb2ec72164c
Harold Affo <affoharold@yahoo.fr>
2018-10-09 11:49:38 -0400

    Fixed redeployment issue

--- hit-core-hl7v2-domain/src/main/java/gov/nist/hit/core/hl7v2/domain/HL7V2TestContext.java
+++ hit-core-hl7v2-domain/src/main/java/gov/nist/hit/core/hl7v2/domain/HL7V2TestContext.java
@@ -17,74 +17,70 @@ import gov.nist.hit.core.domain.VocabularyLibrary;
 @Entity
 public class HL7V2TestContext extends TestContext {
 
+	private static final long serialVersionUID = 1L;
 
-  private static final long serialVersionUID = 1L;
+	private boolean dqa;
 
-  private boolean dqa;
+	@OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(unique = true, nullable = false, insertable = true, updatable = true)
+	@JsonProperty(value = "profile")
+	protected ConformanceProfile conformanceProfile;
 
-  @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-  @JoinColumn(unique = true, nullable = false, insertable = true, updatable = true)
-  @JsonProperty(value = "profile")
-  protected ConformanceProfile conformanceProfile;
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	protected VocabularyLibrary vocabularyLibrary;
 
-  @ManyToOne
-  protected VocabularyLibrary vocabularyLibrary;
+	@JsonIgnore
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	@JoinColumn(nullable = true, insertable = true, updatable = true)
+	protected Constraints constraints;
 
-  @JsonIgnore
-  @ManyToOne
-  protected Constraints constraints;
+	@JsonIgnore
+	@OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(nullable = true, insertable = true, updatable = true)
+	protected Constraints addditionalConstraints;
 
-  @JsonIgnore
-  @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-  @JoinColumn(nullable = true, insertable = true, updatable = true)
-  protected Constraints addditionalConstraints;
+	public HL7V2TestContext() {
+		this.format = "hl7v2";
+	}
 
+	public ConformanceProfile getConformanceProfile() {
+		return conformanceProfile;
+	}
 
-  public HL7V2TestContext() {
-    this.format = "hl7v2";
-  }
+	public void setConformanceProfile(ConformanceProfile conformanceProfile) {
+		this.conformanceProfile = conformanceProfile;
+	}
 
-  public ConformanceProfile getConformanceProfile() {
-    return conformanceProfile;
-  }
+	public Constraints getConstraints() {
+		return constraints;
+	}
 
-  public void setConformanceProfile(ConformanceProfile conformanceProfile) {
-    this.conformanceProfile = conformanceProfile;
-  }
+	public void setConstraints(Constraints constraints) {
+		this.constraints = constraints;
+	}
 
+	public VocabularyLibrary getVocabularyLibrary() {
+		return vocabularyLibrary;
+	}
 
-  public Constraints getConstraints() {
-    return constraints;
-  }
+	public void setVocabularyLibrary(VocabularyLibrary vocabularyLibrary) {
+		this.vocabularyLibrary = vocabularyLibrary;
+	}
 
-  public void setConstraints(Constraints constraints) {
-    this.constraints = constraints;
-  }
+	public Constraints getAddditionalConstraints() {
+		return addditionalConstraints;
+	}
 
-  public VocabularyLibrary getVocabularyLibrary() {
-    return vocabularyLibrary;
-  }
+	public void setAddditionalConstraints(Constraints addditionalConstraints) {
+		this.addditionalConstraints = addditionalConstraints;
+	}
 
-  public void setVocabularyLibrary(VocabularyLibrary vocabularyLibrary) {
-    this.vocabularyLibrary = vocabularyLibrary;
-  }
+	public boolean isDqa() {
+		return dqa;
+	}
 
-  public Constraints getAddditionalConstraints() {
-    return addditionalConstraints;
-  }
-
-  public void setAddditionalConstraints(Constraints addditionalConstraints) {
-    this.addditionalConstraints = addditionalConstraints;
-  }
-
-  public boolean isDqa() {
-    return dqa;
-  }
-
-  public void setDqa(boolean dqa) {
-    this.dqa = dqa;
-  }
-
-
+	public void setDqa(boolean dqa) {
+		this.dqa = dqa;
+	}
 
 }
